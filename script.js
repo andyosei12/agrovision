42fd42c2542a1333daaa8968b534e3b2c8e9357a
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-08-17 16:35:40 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const menuIcon = document.querySelector('.mobile-nav-toggle');
+const closeIcon = document.querySelector('.close-icon');
+const socialIcons = document.querySelector('.nav__mobile-social');
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav__list');
+const navItem = document.querySelectorAll('.nav__list--item');
+
+menuIcon.addEventListener('click', () => {
+  nav.classList.add('nav__mobile');
+  closeIcon.classList.add('close-icon__mobile');
+  navList.classList.add('nav__list-mobile');
+  socialIcons.classList.add('nav__mobile-social--toggle');
+});
+
+closeIcon.addEventListener('click', () => {
+  nav.classList.remove('nav__mobile');
+  closeIcon.classList.remove('close-icon__mobile');
+  navList.classList.remove('nav__list-mobile');
+  socialIcons.classList.remove('nav__mobile-social--toggle');
+});
+
+navItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('nav__mobile');
+    closeIcon.classList.remove('close-icon__mobile');
+    navList.classList.remove('nav__list-mobile');
+    socialIcons.classList.remove('nav__mobile-social--toggle');
+  });
+});
